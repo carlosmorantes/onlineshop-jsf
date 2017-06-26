b60662ece5c9ef8d4573834d32b4476eb33389ed
carrcarl <carrcarl@DEL5484.ppal.directory>
2017-06-26 17:16:51 +0200

    Use faces-config.xml to redirect after a success registration with email and password

---
+++ onlineshop-war/src/de/java2enterprise/onlineshop/RegisterController.java
@@ -0,0 +1,77 @@
+package de.java2enterprise.onlineshop;
+
+import java.io.Serializable;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.transaction.UserTransaction;
+
+@RequestScoped
+@Named
+public class RegisterController implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	//@Inject
+	//private Customer customer;
+	
+	
+	private String testEmail;
+	private String testPassword;
+	
+	
+	@PersistenceUnit
+	private EntityManagerFactory emf;
+	
+	@Resource
+	private UserTransaction ut;
+
+	/*public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}*/
+	
+	public String persist(){
+		try{
+			//ut.begin();
+			//emf.createEntityManager().persist(customer);
+			//ut.commit();
+			//FacesMessage m = new FacesMessage("Succesfully registered!");
+			//FacesContext.getCurrentInstance().addMessage("registerForm", m);
+			return "success";
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return "failure";
+	}
+
+	public String getTestEmail() {
+		return testEmail;
+	}
+
+	public void setTestEmail(String testEmail) {
+		this.testEmail = testEmail;
+	}
+
+	public String getTestPassword() {
+		return testPassword;
+	}
+
+	public void setTestPassword(String testPassword) {
+		this.testPassword = testPassword;
+	}
+
+}
